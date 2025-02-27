fd9e781b0d2e03de924eaa643cbed977e71b6d78
Anand Anil <anand.anil2004@gmail.com>
2025-02-27 15:57:08 +0530

    made FAB for refresh and map. currently not mapped well but will be once map and firebase is integrated

--- Flutter_app/drone_telemetry_and_controls/lib/main.dart
+++ Flutter_app/drone_telemetry_and_controls/lib/main.dart
@@ -41,9 +41,16 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    print("button pressed");
+    print("increment button pressed");
     setState(() {
       _counter++;
+    });
+  }
+
+  void _refresh() {
+    print("reset button pressed");
+    setState(() {
+      _counter=0;
     });
   }
 
@@ -52,20 +59,27 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("Hello"),
-          SizedBox(height: 20,),
-            Center(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text("Hello"),
+            Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.headlineMedium,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          'You have pushed the location button this many times:',
+                        ),
+                        Text(
+                          '$_counter',
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -73,10 +87,22 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.location_pin),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: _refresh,
+            tooltip: "decrement",
+            child:Icon(Icons.refresh),
+          )
+        ],
       ),
     );
   }

60270b5f42560a9a3f66ba665ad976955c7ce31c
Anand Anil <anand.anil2004@gmail.com>
2025-02-27 18:46:25 +0530

    got a working map with location working. when pressed the pin button it will place pin on the map based on user location. useful to make a drone come to me button

--- Flutter_app/drone_telemetry_and_controls/lib/main.dart
+++ Flutter_app/drone_telemetry_and_controls/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,6 +42,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  LatLng? currentlocation;
+  final MapController mapController = MapController();
   int _counter = 0;
 
   void _incrementCounter() {
@@ -50,60 +56,86 @@ class _MyHomePageState extends State<MyHomePage> {
   void _refresh() {
     print("reset button pressed");
     setState(() {
-      _counter=0;
+      _counter = 0;
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return;
+    }
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      currentlocation = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  void _goToCurrentLocation() {
+    if (currentlocation != null) {
+      mapController.move(currentlocation!, 17.5);
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            // Text("Hello"),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          'You have pushed the location button this many times:',
-                        ),
-                        Text(
-                          '$_counter',
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: content(),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
+            onPressed: _goToCurrentLocation,
+            tooltip: 'My Location',
             child: Icon(Icons.location_pin),
           ),
           const SizedBox(height: 16),
           FloatingActionButton(
             onPressed: _refresh,
             tooltip: "decrement",
-            child:Icon(Icons.refresh),
+            child: Icon(Icons.refresh),
           )
         ],
       ),
     );
   }
+
+  Widget content() {
+    return FlutterMap(
+      mapController: mapController,
+      options: MapOptions(
+        initialCenter: LatLng(1.2878, 103.8666),
+        initialZoom: 5,
+      ),
+      children: [
+        openStreetMapTileLayer,
+        if (currentlocation != null)
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: currentlocation!,
+                width: 80,
+                height: 80,
+                child: const Icon(
+                  Icons.location_pin,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              )
+            ],
+          )
+      ],
+    );
+  }
 }
+
+TileLayer get openStreetMapTileLayer => TileLayer(
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+    );
--- Flutter_app/drone_telemetry_and_controls/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter_app/drone_telemetry_and_controls/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import geolocator_apple
 import system_theme
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
 }

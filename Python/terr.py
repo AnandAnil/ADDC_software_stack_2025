0626b2274c1a3612da66b5063fbd7fd227a81a14
TERIN <terinthomas7@gmail.com>
2025-03-22 12:35:45 +0530

    commit ter

---
+++ Python/terr.py
@@ -0,0 +1,19 @@
+from pymavlink import mavutil
+
+
+connection_string = "udp:127.0.0.1:14441"  
+
+# Connect to the drone
+master = mavutil.mavlink_connection(connection_string)
+master.wait_heartbeat()
+print("✅ Drone connected!")
+
+# Fetch and print telemetry data
+while True:
+    msg = master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    if msg:
+        lat = msg.lat / 1e7
+        lon = msg.lon / 1e7
+    
+        print(f"Latitude: {lat}, Longitude: {lon}")
+print(master.messages.keys())
